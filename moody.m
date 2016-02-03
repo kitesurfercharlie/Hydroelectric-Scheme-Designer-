1f99c60fbc2dcc5ce12bc342067395e1209cb5bf
kitesurfercharlie <charlieseviour@gmail.com>
2016-02-03 10:38:13 +0100

    Update moody.m

--- moody.m
+++ moody.m
@@ -17,19 +17,21 @@ end
 if ed>0.05
 warning(sprintf('epsilon/diameter ratio = %f is not on Moody chart',ed));
 end
-if Re<4000, warning('Re = %f in transition range',Re); end
+%if Re<4000, warning('Re = %f in transition range',Re); end
 % --- Use fzero to find f from Colebrook equation.
 % coleFun is an inline function object to evaluate F(f,e/d,Re)
 % fzero returns the value of f such that F(f,e/d/Re) = 0 (approximately)
 % fi = initial guess from Haaland equation, see White, equation 6.64a
 % Iterations of fzero are terminated when f is known to whithin +/- dfTol
-coleFun = inline('1.0/sqrt(f) + 2.0*log10( ed/3.7 + 2.51/( Re*sqrt(f)) )',...
-'f','ed','Re');
-fi = 1/(1.8*log10(6.9/Re + (ed/3.7)^1.11))^2; % initial guess at f
-dfTol = 5e-6;
-f = fzero(coleFun,fi,optimset('TolX',dfTol,'Display','off'),ed,Re);
+
+f = 1/(1.8*log10(6.9/Re + (ed/3.7)^1.11))^2; % initial guess at f
+
+
+for i = 1:10
+  f = (2.0 * log10((ed/3.7) + (2.51/(Re*f^0.5))))^-2;
+end
 % --- sanity check:
 if f<0, error(sprintf('Friction factor = %f, but cannot be negative',f)); 
 
 end
-end
+endfunction
